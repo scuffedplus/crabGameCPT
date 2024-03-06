e2b48b3a088cce757b20b1d3d84aa42679e08937
s1erra <spektor.2007@proton.me>
2024-03-05 20:14:11 -0800

    added terrible enemies

---
+++ Crab Game Godot/enemy.gd
@@ -0,0 +1,24 @@
+extends AnimatedSprite2D
+var i = 0
+const walkdist = 250
+var dir = 1
+var enemNum = 3
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+	
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	i = i+1
+
+	if i <= walkdist:
+		position.x = position.x + dir
+	else:
+		i = 0
+		dir = dir * -1
+	
+		
