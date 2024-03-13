6e51bf1c7245ebbfeef3e4aa3c0767b10097da54
s1erra <spektor.2007@proton.me>
2024-03-13 13:26:37 -0700

    move enemu zoom

--- Crab Game Godot/Enemy Template/EnemyMovement.gd
+++ Crab Game Godot/Enemy Template/EnemyMovement.gd
@@ -6,16 +6,25 @@ var enemNum = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+@onready var animator = $AnimationPlayer
 
+func anim():
+	animator.play("Walking")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	i = i+1
-
-	if i <= walkdist:
-		position.x = position.x + dir
+	if i == walkdist:
+		anim()
 	else:
-		i = 0
-		dir = dir * -1
+		if i > walkdist:
+			i = 0
+		
+#
+#	if i <= walkdist:
+#		position.x = position.x + dir
+#	else:
+#		i = 0
+#		dir = dir * -1
 
 
 func _on_enemy_hitbox_tree_exited():
