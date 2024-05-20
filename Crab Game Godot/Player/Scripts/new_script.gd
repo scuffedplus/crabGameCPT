a55edd20d13580397cf137f475ad0f5755841ec5
DEADLocked90000 <michael.h.gevorkyan@gmail.com>
2024-05-20 13:50:54 -0700

    Delta Update
    Delta is now applied when moving so the speed isn't messed up.

--- Crab Game Godot/Player/Scripts/new_script.gd
+++ Crab Game Godot/Player/Scripts/new_script.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var HoldingNothing
+var Delta = 0
 
 #Player States:
 var Stunned = false
@@ -17,12 +17,12 @@ var Falling = false
 var WalkingMaxSpeed = 600
 var RunningMaxSpeed = 1300
 var CurrentMaxSpeed = WalkingMaxSpeed
-var MaxFallSpeed = 2000
+var MaxFallSpeed = 1750
 
-var Accel = 25
-var Decel = 100
-var Gravity = 35
-var JumpStrength = -2500
+var Accel = 3500
+var Decel = 6000
+var Gravity = 6000
+var JumpStrength = -2750
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,12 +30,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_StatusUpdate()
+	_StatusUpdate(delta)
 	_MovementLogic()
-	
 	move_and_slide()
 
-func _StatusUpdate():
+func _StatusUpdate(delta):
+	Delta = delta
 	if (Input.is_key_pressed(KEY_SHIFT)):
 		Sprinting = true
 		CurrentMaxSpeed = RunningMaxSpeed
@@ -52,7 +52,7 @@ func _StatusUpdate():
 
 func _GravityLogic():
 	if (velocity.y < MaxFallSpeed):
-		velocity.y += Gravity
+		velocity.y += Gravity*Delta
 
 func _MovementLogic():
 	if (Input.is_action_pressed("ui_right")):
@@ -62,7 +62,7 @@ func _MovementLogic():
 			_Decelerate()
 		
 		if (velocity.x <= CurrentMaxSpeed):
-			velocity.x += Accel
+			velocity.x += Accel*Delta
 	elif (Input.is_action_pressed("ui_left")):
 		Idle = false
 		
@@ -70,7 +70,7 @@ func _MovementLogic():
 			_Decelerate()
 		
 		if (velocity.x >= -CurrentMaxSpeed):
-			velocity.x -= Accel
+			velocity.x -= Accel*Delta
 	else :
 		Idle = true
 	
@@ -80,12 +80,13 @@ func _MovementLogic():
 	if (Input.is_key_pressed(KEY_Z) && is_on_floor()):
 		velocity.y = JumpStrength
 	
+	
 func _Decelerate():
 	if (velocity.x != 0):
-		if (abs(velocity.x) <= Decel):
+		if (abs(velocity.x) <= Decel*Delta):
 			velocity.x = 0
 		elif (velocity.x > 0):
-			velocity.x -= Decel
+			velocity.x -= Decel*Delta
 			
 		elif (velocity.x < 0):
-			velocity.x += Decel
+			velocity.x += Decel*Delta
