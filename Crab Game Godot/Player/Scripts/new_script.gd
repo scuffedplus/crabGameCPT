94f7052a45a4faf8e0c7efdbd89cb81954a15562
DEADLocked90000 <michael.h.gevorkyan@gmail.com>
2024-05-30 22:03:17 -0700

    Coyote Time

--- Crab Game Godot/Player/Scripts/new_script.gd
+++ Crab Game Godot/Player/Scripts/new_script.gd
@@ -39,6 +39,8 @@ var Decel = 6000
 var Gravity = 6000
 var JumpStrength = -1850
 #was -2750 before changes to jumping.
+var CoyoteComplete
+
 var ChargePunchTimer
 var Animator
 var PunchHitbox
@@ -78,13 +80,12 @@ func _process(delta):
 				_Punch()
 	
 	if (!GPing):
-		if (!Weightless && !is_on_floor()):
+		if (!Weightless && !is_on_floor() && $CoyoteTime.is_stopped()):
 			_GravityLogic()
 	else:
 		_GPPhysics()
 	
 	_AnimationHandler()
-	
 	move_and_slide()
 
 func _Punch():
@@ -127,6 +128,9 @@ func _StatusUpdate(delta):
 	
 	if (!is_on_floor()):
 		
+		if (!CoyoteComplete):
+			_CoyoteTimeStart()
+		
 		if (GPing):
 			GPing = false
 		
@@ -136,6 +140,7 @@ func _StatusUpdate(delta):
 			Falling = false
 	else:
 		GPable = true
+		CoyoteComplete = false
 		if (StunDelay == false):
 			Stunned = false
 		StunDelay = false
@@ -169,7 +174,7 @@ func _MovementLogic():
 	
 	
 	
-	if (Input.is_action_just_pressed("ui_up") && is_on_floor()):
+	if (Input.is_action_just_pressed("ui_up") && (is_on_floor() or !$CoyoteTime.is_stopped())):
 		velocity.y = JumpStrength
 		Weightless = true
 		$JumpTimer.start()
@@ -237,6 +242,9 @@ func _AnimationHandler():
 	
 	pass
 
+func _CoyoteTimeStart():
+	$CoyoteTime.start()
+	CoyoteComplete = true
 
 func _on_punch_hitbox_area_entered(area):
 	if (area.is_in_group("Enemies")):
